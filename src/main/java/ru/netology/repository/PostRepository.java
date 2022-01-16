469fa6e5a4f6e795f24f3c382187ece165a342c7
Kenyo-cell <rekurt3000@gmail.com>
2022-01-16 19:59:09 +0300

    feat: ended spring web mvc project with stable tomcat 9.0.56 and java servlet api

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostRepository {
+    public List<Post> all();
+    public Optional<Post> getById(long id);
+    public Post save(Post post);
+    public boolean removeById(long id);
+}
---
+++ src/main/java/ru/netology/service/PostService.java
@@ -0,0 +1,46 @@
+package ru.netology.service;
+
+import org.springframework.stereotype.Service;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import ru.netology.repository.PostRepository;
+
+import java.util.List;
+
+@Service
+public class PostService {
+  private final PostRepository repository;
+
+  public PostService(PostRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Post> all() {
+    return repository.all();
+  }
+
+  public Post getById(long id) throws NotFoundException {
+    try {
+      return repository.getById(id).orElseThrow(NotFoundException::new);
+    } catch (NotFoundException e) {
+      throw new NotFoundException("Post not found with id " + id);
+    }
+  }
+
+  public Post save(Post post) throws NotFoundException {
+    try {
+      return repository.save(post);
+    } catch (NotFoundException e) {
+      throw new NotFoundException("Post not found with id " + post.getId());
+    }
+  }
+
+  public void removeById(long id) throws NotFoundException {
+    try {
+      repository.removeById(id);
+    } catch (NotFoundException e) {
+      throw new NotFoundException("Post not found with id " + id);
+    }
+  }
+}
+
